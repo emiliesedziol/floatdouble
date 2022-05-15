187ade8f505efe0c747b7645e404fa68ebac4572
emiliesedziol <sedziole@gmail.com>
2022-05-15 15:26:02 -0400

    Initial commit

---
+++ src/doublefloat.java
@@ -0,0 +1,25 @@
+public class doublefloat {
+
+    public static void main(String[] args) {
+
+        float minFloatValue = Float.MIN_VALUE;
+        float maxFloatValue = Float.MAX_VALUE;
+
+        System.out.println(("Float min value = " + minFloatValue));
+        System.out.println("Float max value n= " + maxFloatValue );
+
+        double minDoubleValue = Double.MIN_VALUE;
+        double maxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double min value = " + minDoubleValue);
+        System.out.println("Double max value n= " + maxDoubleValue );
+
+        int  intValue = 5;
+        float floatValue = 5.27864f;
+        double doubleValue= 5.24d;
+
+        System.out.println("int " + intValue);
+        System.out.println("Float " + floatValue);
+        System.out.println("Double " + doubleValue);
+    }
+}
